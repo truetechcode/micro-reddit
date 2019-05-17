4c8b6a7ad5d53170c914c00a6ab5e04e97c6ea96
Oghenebrume50 <raphlbrume@gmail.com>
2019-05-17 13:50:50 +0100

    setup user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :name, presence: true, uniqueness: true, length: { in: 4..12 }
+    validates :email, presence: true, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,17 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(name: "Rpahael sobis", email: "foo@bar.com")
+  end 
+
+  test "should be valid" do
+    assert @user.valid?
+  end 
+
+  test "name should not be present" do
+    @user.name = "         "
+    assert_not @user.valid?
+  end
 end
