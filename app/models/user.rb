f2df4e5b26813b60fe4c5db572257ec4ceb941b2
TERVER AOSU <terveraosu@gmail.com>
2019-05-17 15:33:44 +0100

    Created Post Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     validates :name, presence: true, uniqueness: true, length: { in: 4..12 }
     validates :email, presence: true, uniqueness: true
+
+    has_many :post
 end
