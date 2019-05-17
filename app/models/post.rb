3d1d94db0c57a102574821a706c68d1b30a90360
Oghenebrume50 <raphlbrume@gmail.com>
2019-05-17 21:53:07 +0100

    validating comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
     validates :title, presence: true, uniqueness: true
     validates :body, presence: true
 
+    has_many :comment 
     belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_134915) do
+ActiveRecord::Schema.define(version: 2019_05_17_152928) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "pin_Id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
